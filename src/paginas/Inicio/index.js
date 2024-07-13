dc9dbf496dde0c60923e95bf38d395dac5497984
Ariel Spencer <arielspencer.tech@gmail.com>
2024-07-12 23:48:49 -0300

    Implementar menu com a biblioteca React Router

---
+++ src/paginas/Inicio/index.js
@@ -0,0 +1,7 @@
+function Inicio() {
+    return (
+      <h2>Página de Inicio</h2>
+    );
+  }
+  
+  export default Inicio;
---
+++ src/paginas/SobreMim/index.js
@@ -0,0 +1,7 @@
+function SobreMim() {
+    return (
+      <h2>Página Sobre Mim</h2>
+    );
+  }
+  
+  export default SobreMim;
