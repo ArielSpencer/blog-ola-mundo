37d82d2ddd62d7a8bd3b30d9de9cb081382bee4b
Ariel Spencer <arielspencer.tech@gmail.com>
2024-07-14 12:54:08 -0300

    Implementar página de artigos do blog

--- src/componentes/Post/index.js
+++ src/componentes/Post/index.js
@@ -1,16 +1,33 @@
-import styles from "./Post.module.css";
+import "./Post.css";
+import { useParams } from "react-router-dom";
+import posts from "json/posts.json";
+import PostModelo from "componentes/PostModelo";
+import ReactMarkdown from "react-markdown";
 
-export default function Post({ post }) {
+
+export default function Post() {
+    const parametros = useParams();
+
+    const post = posts.find((post) => {
+        return post.id === Number(parametros.id);
+    })
+
+    if (!post) {
+        return <h1>Post não encontrado.</h1>
+    }
+
     return (
-        <div className={styles.post}>
-            <img className={styles.capa}
-                src={`/assets/posts/${post.id}/capa.jpg`}
-                alt="Imagem de capa do post"
-            />
+        <PostModelo
+            fotoCapa={`/assets/posts/${post.id}/capa.png`}
+            titulo={post.titulo}
+        >
 
-            <h2 className={styles.titulo}>{post.titulo}</h2>
+            <div className="post-markdown-container">
+                <ReactMarkdown>
+                    {post.texto}
+                </ReactMarkdown>
+            </div>
 
-            <button className={styles.botaoLer}>Ler</button>
-        </div>
+        </PostModelo>
     )
 }
